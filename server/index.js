f015a1b9186c352ba57f54cfa57d93a4c7ffeae6
Yo-Joshwa <shivambaberwal123@gmail.com>
2025-07-03 19:45:18 +0530

    updated stats

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ import userStatsRoutes from "./routes/userStatsRoutes.js";
 import userRoutes from "./routes/user.js";
 import categoriesRoutes from "./routes/categories.js";
 import clerkWebhook from "./routes/clerkWebhook.js";
-import apiRoutes from "./routes/api.js";
 import { ClerkExpressWithAuth } from "@clerk/clerk-sdk-node";
 import protectedRoutes from "./routes/protected.js";
 import quizRoutes from "./routes/quiz.js";
@@ -29,7 +28,6 @@ mongoose
 
 app.use("/api/user", userRoutes);
 app.use("/api/user", userQuizRoutes);
-app.use("/api", apiRoutes);
 app.use("/api/categories", categoriesRoutes);
 app.use("/api/categories", quizRoutes);
 app.use("/api", userStatsRoutes);
