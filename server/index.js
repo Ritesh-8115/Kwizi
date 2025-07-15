86cbcae13730238eb60d38c07e1c03e632d06442
Yo-Joshwa <shivambaberwal123@gmail.com>
2025-07-15 09:11:06 +0530

    deployment done

--- server/index.js
+++ server/index.js
@@ -13,48 +13,48 @@ import protectedRoutes from "./routes/protected.js";
 import quizRoutes from "./routes/quiz.js";
 import userQuizRoutes from "./routes/userQuizRoutes.js";
 
+
+// MongoDB connection
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(() => console.log("MongoDB connected"))
+.catch((error) => console.log(error));
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [process.env.CLIENT_URL];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "x-user-id",
       "Access-Control-Allow-Origin",
       "Content-Type",
       "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
     ],
     credentials: true,
   })
 );
 app.use(express.json());
 
-// MongoDB connection
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.log(error));
 
+
+app.use(
+  ClerkExpressWithAuth({
+    secretKey: process.env.CLERK_SECRET_KEY,
+  })
+);
 app.use("/api/user", userRoutes);
 app.use("/api/user", userQuizRoutes);
 app.use("/api/categories", categoriesRoutes);
 app.use("/api/categories", quizRoutes);
 app.use("/api", userStatsRoutes);
 app.use("/api", clerkWebhook);
-app.use(
-  ClerkExpressWithAuth({
-    secretKey: process.env.CLERK_SECRET_KEY,
-  })
-);
+
 app.use("/api", protectedRoutes);
 
-app.listen(5000, () => console.log(`Server is now running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
